cb57ed56e04d09ae9bf9c65b5ee587cbaa81d82f
Anya <amanbkumar123@gmail.com>
2026-09-14 00:14:43 +0530

    feat: expose local accessibility action bridge

--- app/src/main/java/com/anya/luna/AgentAccessibilityService.kt
+++ app/src/main/java/com/anya/luna/AgentAccessibilityService.kt
@@ -4,10 +4,25 @@ import android.accessibilityservice.AccessibilityService
 import android.view.accessibility.AccessibilityEvent
 
 class AgentAccessibilityService : AccessibilityService() {
+    override fun onServiceConnected() {
+        super.onServiceConnected()
+        instance = this
+    }
+
     override fun onAccessibilityEvent(event: AccessibilityEvent?) { }
     override fun onInterrupt() { }
+
+    override fun onDestroy() {
+        if (instance === this) instance = null
+        super.onDestroy()
+    }
 
     fun performBack(): Boolean = performGlobalAction(GLOBAL_ACTION_BACK)
     fun performHome(): Boolean = performGlobalAction(GLOBAL_ACTION_HOME)
     fun performRecents(): Boolean = performGlobalAction(GLOBAL_ACTION_RECENTS)
+
+    companion object {
+        @Volatile var instance: AgentAccessibilityService? = null
+            private set
+    }
 }
